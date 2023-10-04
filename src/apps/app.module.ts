ba515b04a9addf0261dd692860b041d84251d5d1
Leonardorossato <leonardorossato21@gmail.com>
2023-10-04 10:37:13 -0300

    correções e modulo de auth criado

--- src/apps/app.module.ts
+++ src/apps/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: []
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule],
 })
 export class AppModule {}
