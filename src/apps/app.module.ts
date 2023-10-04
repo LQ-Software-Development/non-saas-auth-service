7c6ca4e7f5072e1415ac5c200c949d7fc92b85a0
Leonardorossato <leonardorossato21@gmail.com>
2023-10-04 13:12:30 -0300

    correção nos modulos e  imports dos modulos que faltavam

--- src/apps/app.module.ts
+++ src/apps/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './auth/database/database.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
--- src/apps/auth/auth.module.ts
+++ src/apps/auth/auth.module.ts
@@ -1,4 +1,36 @@
 import { Module } from '@nestjs/common';
+import { LoginUserController } from './usecases/commands/login-user/login-user.controller';
+import { LoginUserUseCase } from './usecases/commands/login-user/login-user.usecase';
+import { UserRepository } from './repositories/implements/user.respository';
+import { JwtService } from '@nestjs/jwt';
+import { userSchemaProviders } from './database/providers/schema/user.schema';
+import { databaseProviders } from './database/providers/database.provider';
+import { RegisterUserController } from './usecases/commands/register-user/register-user.controller';
+import { RegisterUserUseCase } from './usecases/commands/register-user/register-user.usecase';
+import { ChangeUserPassowrdUseCase } from './usecases/commands/change-user-password/change-user-password.usecase';
+import { ChangeUserPassowrdController } from './usecases/commands/change-user-password/change-user-password.controller';
 
-@Module({})
+@Module({
+  controllers: [LoginUserController, RegisterUserController, ChangeUserPassowrdController],
+  providers: [
+    LoginUserUseCase,
+    RegisterUserUseCase,
+    ChangeUserPassowrdUseCase,
+    {
+      provide: 'user-repository',
+      useClass: UserRepository,
+    },
+    {
+      provide: 'jwt-service',
+      useFactory: () => {
+        return new JwtService({
+          secret: process.env.JWT_SECRET,
+          signOptions: { expiresIn: '1d' },
+        });
+      },
+    },
+    ...userSchemaProviders,
+    ...databaseProviders,
+  ],
+})
 export class AuthModule {}
