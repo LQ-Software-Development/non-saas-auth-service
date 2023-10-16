16b325cc0e2a1c267a1757111f36e6cc98b5c49e
Leonardorossato <leonardorossato21@gmail.com>
2023-10-16 11:08:55 -0300

    implementação nodemailer funcionando no gmail, so consertar a url

--- src/apps/auth/auth.module.ts
+++ src/apps/auth/auth.module.ts
@@ -1,18 +1,17 @@
 import { Module } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { databaseProviders } from './database/providers/database.provider';
+import { emailProvider } from './database/providers/email/email.provider';
+import { userSchemaProviders } from './database/providers/schema/user.schema';
+import { UserRepository } from './repositories/implements/user.respository';
+import { ChangeUserPassowrdController } from './usecases/commands/change-user-password/change-user-password.controller';
+import { ChangeUserPassowrdUseCase } from './usecases/commands/change-user-password/change-user-password.usecase';
 import { LoginUserController } from './usecases/commands/login-user/login-user.controller';
 import { LoginUserUseCase } from './usecases/commands/login-user/login-user.usecase';
-import { UserRepository } from './repositories/implements/user.respository';
-import { JwtService } from '@nestjs/jwt';
-import { userSchemaProviders } from './database/providers/schema/user.schema';
-import { databaseProviders } from './database/providers/database.provider';
 import { RegisterUserController } from './usecases/commands/register-user/register-user.controller';
 import { RegisterUserUseCase } from './usecases/commands/register-user/register-user.usecase';
-import { ChangeUserPassowrdUseCase } from './usecases/commands/change-user-password/change-user-password.usecase';
-import { ChangeUserPassowrdController } from './usecases/commands/change-user-password/change-user-password.controller';
 import { RequestResetEmailController } from './usecases/commands/reset-password/request-reset-email/request-reset-email.controller';
 import { RequestResetEmailUseCase } from './usecases/commands/reset-password/request-reset-email/request-reset-email.usecase';
-import { emailProvider } from './database/providers/email/email.provider';
-import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
   controllers: [
@@ -36,21 +35,6 @@ import { MailerModule } from '@nestjs-modules/mailer';
         return new JwtService({
           secret: process.env.JWT_SECRET,
           signOptions: { expiresIn: '1d' },
-        });
-      },
-    },
-    {
-      provide: 'email-provider',
-      useFactory: () => {
-        MailerModule.forRoot({
-          transport: {
-            host: process.env.NODEMAILER_HOST,
-            port: process.env.NODEMAILER_PORT,
-            auth: {
-              user: process.env.NODEMAILER_USER,
-              pass: process.env.NODEMAILER_PASSWORD,
-            },
-          },
         });
       },
     },
--- src/apps/auth/database/providers/email/email.provider.ts
+++ src/apps/auth/database/providers/email/email.provider.ts
@@ -1,3 +1,5 @@
+import * as nodemailer from 'nodemailer';
+
 export const emailProvider = [
   {
     provide: 'EMAIL_PROVIDER',
@@ -5,11 +7,36 @@ export const emailProvider = [
       transport: {
         host: process.env.NODEMAILER_HOST,
         port: process.env.NODEMAILER_PORT,
+        secure: false,
         auth: {
           user: process.env.NODEMAILER_USER,
           pass: process.env.NODEMAILER_PASSWORD,
         },
       },
+      defaults: {
+        from: '',
+      },
     },
   },
 ];
+
+export const sendEmail = async (data) => {
+  const transport = nodemailer.createTransport({
+    host: process.env.NODEMAILER_HOST,
+    port: process.env.NODEMAILER_PORT,
+    secure: false,
+    auth: {
+      user: process.env.NODEMAILER_USER,
+      pass: process.env.NODEMAILER_PASSWORD,
+    },
+  });
+  const info = await transport.sendMail({
+    from: 'Hey <abc@gmail.com>',
+    to: data.to,
+    subject: data.subject,
+    text: data.text,
+    html: data.html,
+  });
+  console.log(info.messageId);
+  console.log(nodemailer.getTestMessageUrl(info));
+};
