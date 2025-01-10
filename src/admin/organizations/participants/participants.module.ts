274cfb9294611271b99dd479161a0225546f8012
Lucas Quadrini <61626249+QuadriniL@users.noreply.github.com>
2025-01-10 16:32:04 -0300

    feat(participants): integrate Mongoose for participant entity management
    - Added MongooseModule to ParticipantsModule for MongoDB integration.
    - Defined Participant entity and schema to facilitate data modeling.
    - Enhanced the module's imports to support participant data operations.

--- src/admin/organizations/participants/participants.module.ts
+++ src/admin/organizations/participants/participants.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ParticipantsController } from './participants.controller';
 import { ListOrganizationParticipantsService } from './services/list-organization-participants.service';
 import { CreateOrganizationParticipantsService } from './services/create-organization-participants.service';
+import {
+  Participant,
+  ParticipantSchema,
+} from 'src/organizations/participants/entities/participant.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Participant.name, schema: ParticipantSchema },
+    ]),
+  ],
   controllers: [ParticipantsController],
   providers: [
     CreateOrganizationParticipantsService,
